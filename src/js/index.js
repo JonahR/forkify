b1b17033af26a300139587c43901ab6d935376b9
Jonah Rueb <ruebjonah@gmail.com>
2019-07-07 21:29:10 -0500

    Recipe is rendered to UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global State of the app
@@ -63,6 +64,8 @@ const controlRecipe =  async () => {
 
     if (id) {
         // Prepare the UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
 
         // Create a new recipe object
         state.recipe = new Recipe(id);
@@ -77,7 +80,8 @@ const controlRecipe =  async () => {
             state.recipe.calcServings();
 
             // Render the recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (err) {
             alert('Error processing recipe!');
         }
